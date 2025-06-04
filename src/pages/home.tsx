c47d3c4d7c89d347c3dc48577f83ec0e6aefb9dd
Anroux <anrouxb314@gmail.com>
2025-06-04 21:50:13 +0200

    added page with all the users from the API

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,29 +1,62 @@
-import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import UserCard from "../components/UserCard";
+import { getUsers } from "../services/githubServices";
+import type { User } from "../models/user";
+import { useDebounce } from "use-debounce";
+import Loader from "../components/Loader";
 
 const Home = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchValue] = useDebounce(searchTerm, 500);
+  const [isDataLoading, setIsDataLoading] = useState(false);
+
+  const onSearch = (value: string): void => {
+    setSearchTerm(value);
+  };
+
+  useEffect(() => {
+    setIsDataLoading(true);
+    (async () => {
+      const _users = await getUsers(debouncedSearchValue);
+      setUsers(_users);
+      setIsDataLoading(false);
+    })();
+  }, [debouncedSearchValue]);
+
   return (
     <>
-      <Header />
-      <SearchBar />
-      <UserCard />
-      <div className="flex">
-        <a
-          href="#"
-          className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-        >
-          Previous
-        </a>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header />
+          <SearchBar onSearch={onSearch} />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+            {users.map((user) => (
+              <UserCard user={user} />
+            ))}
+          </div>
+          <div className="flex">
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 mt-5 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Previous
+            </a>
 
-        <a
-          href="#"
-          className="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-        >
-          Next
-        </a>
-      </div>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 mt-5 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Next
+            </a>
+          </div>
+        </div>
+      )}
     </>
   );
 };
---
+++ src/pages/user-profile.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import type { User } from "../models/user";
+import { getUser } from "../services/githubServices";
+import Loader from "../components/Loader";
+import { DiVim } from "react-icons/di";
+
+const UserProfile = () => {
+  const { login } = useParams();
+  const [isDataLoading, setIsDataLoading] = useState(false);
+  const [user, setUser] = useState<User[]>([]);
+
+  useEffect(() => {
+    setIsDataLoading(true);
+    (async () => {
+      const username = login ? login : "";
+      const _user = await getUser(username);
+      setUser(_user);
+      setIsDataLoading(false);
+    })();
+  }, [login]);
+
+  return (
+    <>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <p>{user.login}</p>
+          <img
+            className="w-24 h-24 mb-3 rounded-full shadow-lg"
+            src={user.avatar_url}
+            alt="Profile"
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UserProfile;
