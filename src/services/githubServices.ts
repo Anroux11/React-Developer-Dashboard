7145db1599fe9afdab94b39fa2c071f254bbbbe2
Anroux <anrouxb314@gmail.com>
2025-06-05 14:58:51 +0200

    updated files

--- src/services/githubServices.ts
+++ src/services/githubServices.ts
@@ -1,51 +1,59 @@
-import axios from 'axios'
+import axios from "axios";
 
 const token: string = `${import.meta.env.VITE_TOKEN}`;
 
 const axiosInstance = axios.create({
-    baseURL: 'https://api.github.com',
-    headers: {
-        Authorization: `token ${token}`
-    }
-})
+  baseURL: "https://api.github.com",
+  headers: {
+    Authorization: `token ${token}`,
+  },
+});
 
 export const getUsers = async (search?: string) => {
-    const searchParams = new URLSearchParams();
-    if (search) {
-        searchParams.set('q', search);
-    }
-    let url = '/users';
-    if (search) {
-        url = `/search/users?${searchParams.toString()}`
-    }
-    const data = await axiosInstance.get(url)
-        .then((response) => {
-            if (response.data.items) {
-                return response.data.items;
-            }
-            return response.data ;
-        }, (err) => {
-            console.error(err);
-        }).catch((err) => {
-            console.error(err);
-        });
-    return data;
-}
-
+  const searchParams = new URLSearchParams();
+  if (search) {
+    searchParams.set("q", search);
+  }
+  let url = "/users";
+  if (search) {
+    url = `/search/users?${searchParams.toString()}`;
+  }
+  const data = await axiosInstance
+    .get(url)
+    .then(
+      (response) => {
+        if (response.data.items) {
+          return response.data.items;
+        }
+        return response.data;
+      },
+      (err) => {
+        console.error(err);
+      }
+    )
+    .catch((err) => {
+      console.error(err);
+    });
+  return data;
+};
 
 export const getUser = async (username: string) => {
-    const url = `/users/${username}`
-    const data = await axiosInstance.get(url)
-        .then((response) => {
-            if (response.data.items) {
-                return response.data.items;
-            }
-            return response.data ;
-        }, (err) => {
-            console.error(err);
-        }).catch((err) => {
-            console.error(err);
-        });
-    return data;
-}
-
+  const url = `/users/${username}`;
+  const data = await axiosInstance
+    .get(url)
+    .then(
+      (response) => {
+        if (response.data.items) {
+          return response.data.items;
+        }
+        return response.data;
+      },
+      (err) => {
+        console.error(err);
+      }
+    )
+    .catch((err) => {
+      console.error(err);
+    });
+  return data;
+};
